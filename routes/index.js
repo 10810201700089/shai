c4f4c60918711216663e16247076a7566078af9d
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-17 00:53:33 +0200

    add someFunciton

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+function someFucntion(){
+  return "hello world";
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send(someFucntion())
